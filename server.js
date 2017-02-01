5916345d47cf5c44a3a5d6c1424ff0983661fa17
shivamd20 <shivamd20@gmail.com>
2017-02-02 05:01:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('artcle-one', function (req, res) {
+  res.send('this is the ariticle one' );
+});
+
+app.get('article-two', function (req, res) {
+  res.send('this is the article two');
+});
+
+app.get('article-three', function (req, res) {
+  res.send('this is the article three');
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
