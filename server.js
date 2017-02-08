ded0938d8e76671aa62b0299ad6a0418107584a7
shivamd20 <shivamd20@gmail.com>
2017-02-08 21:30:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ function createTemplate(data)
         var htmlTemplate=
             `<html>
           <head>
-            <title> $(title) </title>
+            <title> ${title} </title>
             <meta name="viewport" content="width-device-width, initial-scale-1" /> 
           </head>
         
@@ -42,13 +42,14 @@ function createTemplate(data)
                     </div>
                     <hr/>
                     <h3> 
-                    $(heading)
+                    
+                         ${heading}
                     </h3>
                     <div>
                         $(date)
                     </div>
                     <div>
-                       $(content)
+                       ${content}
                     </div>
                 </div>
           </body>
