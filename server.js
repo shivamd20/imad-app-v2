a4ebc60b4767f9101713b4994ee15f2a2a3f659f
shivamd20 <shivamd20@gmail.com>
2017-02-08 21:52:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,6 +88,9 @@ app.get('/', function (req, res) {
 
 
 app.get('/:articleName', function (req, res) {
+    
+    var articleName=req.params.articleName;
+    
   res.send(articles[articleName]);
 });
 
