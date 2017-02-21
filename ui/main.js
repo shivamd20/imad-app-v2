a0f807a60d054d45d01c50a6156615587940793e
shivamd20 <shivamd20@gmail.com>
2017-02-21 23:53:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
 
-var element =document.getElementById('main-text');
+//var element =document.getElementById('main-text');
 
-element.innerHTML="new Value";
+//element.innerHTML="new Value";
