d01f53478df0b6c0453afb193010a4b35ecf3f8a
shivamd20 <shivamd20@gmail.com>
2017-03-04 14:27:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element =document.getElementById('main-text');
 
 element.innerHTML="new Value";
 
-var img=document.getElementById('img')
+var img=document.getElementById('madi')
 
 img.onClick = function()
 {
